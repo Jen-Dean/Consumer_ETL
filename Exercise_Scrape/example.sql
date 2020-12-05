eb9fa2432780e831c2908d1404013d0809f66818
Jen-Dean <missjenniferdean@gmail.com>
2020-12-05 16:01:33 -0500

    Cleaning up the files

--- Exercise_Scrape/example.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE exercises (
-	exercise_id SERIAL PRIMARY KEY NOT NULL,
-	"exercise (calories burned per min)" VARCHAR NOT NULL,
-	"130 lbs" DECIMAL NOT NULL,
-	"155 lbs" DECIMAL NOT NULL,
-	"180 lbs" DECIMAL NOT NULL,
-	"205 lbs" DECIMAL NOT NULL
-
-)
