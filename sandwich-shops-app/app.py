257770893c53c56b50c93539d62f15cc91376c5f
Veena Uppalapati <vuveenauppalapati@gmail.com>
2020-12-05 17:33:36 -0500

    Load results in json format

--- sandwich-shops-app/app.py
+++ sandwich-shops-app/app.py
@@ -9,7 +9,7 @@ from flask import Flask, jsonify
 # Database Setup
 #################################################
 # engine = create_engine("sqlite:///titanic.sqlite")
-rds_connection_string = f"postgres:{postgres_pwd}@localhost:5432/programming_db"
+rds_connection_string = f"postgres:{postgres_pwd}@localhost:5432/for_now"
 engine = create_engine(f'postgresql://{rds_connection_string}')
 
 #################################################
@@ -28,13 +28,26 @@ def home():
 
 @app.route("/api")
 def api():
-    results = engine.execute("select * from  programming_languages")
+    results = engine.execute("select * from  sandwiches")
     results_lst = []
     for result in results:
         results_dict ={
-            'id' : result[0],
-            'language' : result[1],
-            'rating': result[2]
+            'Restaurant' : result[1],
+            'Sandwich_Name' : result[2],
+            'Bread_Type' : result[3],
+            'Sandwich_Lenghth' : result[4],
+            'Sandwich_Description' : result[5],
+            'Calories' : result[6],
+            'Calories_From_Fat' : result[7],
+            'Total_Fat' : result[8],
+            'Protein' : result[9],
+            'Saturated_Fat' : result[10],
+            'Trans_Fat' : result[11],
+            'Cholesterol' : result[12],
+            'Sodium' : result[13],
+            'Total_Carbs' :result[14],
+            'Dietary_Fiber' : result[15],
+            'Sugars' : result[16]
         }
         results_lst.append(results_dict)
     return jsonify(results_lst)
